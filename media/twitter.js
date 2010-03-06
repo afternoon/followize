74c73923922ce936c147db121b1a8a6e29de0398
Ben Godfrey <ben@ben2.com>
2010-03-06 19:36:44 +0000

    Separate Twitter JS module into it's own file

---
+++ media/twitter.js
@@ -0,0 +1,84 @@
+/*
+ *  Twitter
+ *
+ *  Module for accessing Twitter's API via OAuth-signed JSONP requests. Only
+ *  supports read access as JSONP is restricted to GET requests.
+ */
+
+// use console.log if available, silently drop log messages if not
+var log = function(s) { return; };
+if (typeof console !== "undefined") { log = function(s) { console.log(s) }; }
+
+// twitter module - just a namespace
+var twitter = {
+    base: "https://twitter.com",
+
+    oauthToken: "",
+    oauthTokenSecret: "",
+    oauthConsumerKey: "",
+
+    init: function(params) {
+        twitter.oauthToken = params.oauthToken || "";
+        twitter.oauthConsumerKey = params.oauthConsumerKey || "";
+    },
+
+    // sign ajax request params
+    sign: function(params) {
+        var message = {
+            action:     params.url,
+            method:     "GET",
+            parameters: params.data
+        };
+
+        OAuth.setTimestampAndNonce(message);
+
+        var accessor = {
+            token:          twitter.oauthToken,
+            consumerKey:    twitter.oauthConsumerKey
+        };
+        
+        OAuth.completeRequest(message, accessor);
+
+        // update the Ajax request to add oauth_ parameters
+        params.data = OAuth.getParameterMap(message.parameters);
+        return params;
+    },
+
+    // request data from Twitter via JSONP and pass data to callback
+    load: function(params) {
+        params.url = params.url + "?callback=?";
+        var signedParams = twitter.sign(params);
+        return $.jsonp(signedParams);
+    },
+
+    // recursively get all following 100 at a time, fire callback for each 100
+    following: function(handlePage, finished, cursor) {
+        var c = cursor || -1;
+        var fin = finished || function() { log("Loaded all following"); };
+        var followingSuccess = function(data, textStatus) {
+            log({loaded: data});
+            handlePage(data.users);
+            if (data.next_cursor !== 0) {
+                twitter.following(handlePage, fin, data.next_cursor);
+            }
+            else {
+                fin();
+            }
+        };
+        var followingError = function(params, textStatus) {
+            log("Error: " + textStatus);
+        }
+        twitter.load({
+            url:        twitter.base + "/statuses/friends.json",
+            data:       {cursor: c},
+            success:    followingSuccess,
+            error:      followingError
+        });
+    },
+
+    // comparison function to sort and array of tweets latest first
+    compareTweetTimesDesc: function(a, b) {
+        var userTime = function(u) { return new Date(u["status"] && u["status"].created_at || u.created_at) };
+        return userTime(b) - userTime(a);
+    }
+};
